b6db326a050187c2a16ed4871847a580f8211491
Housna <185698237+housna11@users.noreply.github.com>
2026-07-10 22:34:57 +0100

    Mise à jour connexion/inscription

--- frontend/app/connexion/page.tsx
+++ frontend/app/connexion/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+/* eslint-disable @next/next/no-img-element */
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import Link from "next/link";
@@ -17,7 +18,7 @@ const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     toast.success("Connexion réussie !");
     router.push("/");
   } catch (error) {
-    toast.error("Email ou mot de passe incorrect.");;
+    toast.error("Email ou mot de passe incorrect");
   }
 };
 
@@ -26,53 +27,57 @@ const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-[#eaf4ff] via-white to-[#eaf4ff] px-4">
-      <div className="w-full max-w-md transform transition duration-300 hover:scale-[1.02]">
-        <div className="bg-white rounded-2xl shadow-xl p-8 border border-blue-100">
-          <h2 className="text-3xl font-bold text-center text-[#0474C4] mb-6">Connexion</h2>
-          <form onSubmit={handleSubmit} className="space-y-4">
-            <div>
-              <label className="block text-sm font-medium text-[#0474C4] mb-1">
-                Email
-              </label>
-              <input
-                type="email"
-                name="email"
-                placeholder="exemple@email.com"
-                onChange={handleChange}
-                className="w-full px-4 py-3 border border-gray-200 rounded-lg 
-                focus:outline-none focus:ring-2 focus:ring-[#0474C4]"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-sm font-medium text-[#0474C4] mb-1">
-                Mot de passe
-              </label>
-              <input
-                type="password"
-                name="password"
-                placeholder="••••••••"
-                onChange={handleChange}
-                className="w-full px-4 py-3 border border-gray-200 rounded-lg 
-                focus:outline-none focus:ring-2 focus:ring-[#0474C4]"
-                required
-              />
-            </div>
-            <button
-              className="w-full bg-[#0474C4] cursor-pointer text-white py-3 rounded-lg hover:bg-[#023e73] transition shadow-md">
-              Se connecter
-            </button>
-          </form>
+  <div className=" bg-gradient-to-br mt-34 ">
+    <div className="flex flex-col md:flex-row w-full max-w-5xl mx-auto shadow-xl rounded-2xl overflow-hidden bg-white">
 
-          <p className="text-sm text-center mt-5 text-gray-600">
-            Pas de compte ?{" "}
-            <Link href="/inscription" className="text-[#0474C4] font-semibold underline">S&#39;inscrire           </Link>
-          </p>
-
-        </div>
+      <div className="md:w-1/2">
+        <img
+          src="/found.jpg"
+          alt="found"
+          className="w-full h-full object-cover"/>
+      </div>
+      <div className="md:w-1/2 p-8 flex flex-col justify-center">
+        <h2 className="text-3xl font-bold text-center text-[#0474C4] mb-6">Connexion</h2>
+        <p className="text-center text-gray-500 mb-6">Connectez-vous à votre compte Lost & Found</p>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label className="block text-sm font-medium text-[#0474C4] mb-1">Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="exemple@email.com"
+              onChange={handleChange}
+              className="w-full px-4 py-3 border border-gray-200 rounded-lg
+              focus:outline-none focus:ring-2 focus:ring-[#0474C4]"
+              required/>
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-[#0474C4] mb-1">Mot de passe</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="••••••••"
+              onChange={handleChange}
+              className="w-full px-4 py-3 border border-gray-200 rounded-lg
+              focus:outline-none focus:ring-2 focus:ring-[#0474C4]"
+              required/>
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-[#0474C4] cursor-pointer text-white py-3 rounded-lg hover:bg-[#023e73] transition shadow-md">
+            Se connecter
+          </button>
+        </form>
+        <p className="text-sm text-center mt-5 text-gray-600">
+          Pas de compte ?{" "}
+          <Link
+            href="/inscription"
+            className="text-[#0474C4] font-semibold underline">S&#39;inscrire
+          </Link>
+        </p>
       </div>
     </div>
-  );
+  </div>
+);
 };
 
--- frontend/app/inscription/page.tsx
+++ frontend/app/inscription/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+/* eslint-disable @next/next/no-img-element */
 import { useState } from "react";
 import { register } from "../Services/auth";
 import Link from "next/link";
@@ -29,84 +30,90 @@ export default function Inscription() {
     console.error(error);
   }
   };
-
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-[#eaf4ff] via-white to-[#eaf4ff] px-4">
-      <div className="w-full max-w-md transform transition duration-300 hover:scale-[1.02]">
-        <div className="bg-white rounded-2xl shadow-xl p-8 border border-blue-100">
-          <h2 className="text-3xl font-bold text-center text-[#0474C4] mb-6">Inscription</h2>
-          <form onSubmit={handleSubmit} className="space-y-4">
-            <div>
-              <label className="block text-sm font-medium text-[#0474C4] mb-1">
-                Nom complet
-              </label>
-              <input
-                type="text"
-                name="name"
-                placeholder="Entrez votre nom"
-                onChange={handleChange}
-                className="w-full px-4 py-3 border border-gray-200 rounded-lg 
-                focus:outline-none focus:ring-2 focus:ring-[#0474C4]"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-sm font-medium text-[#0474C4] mb-1">
-                Email
-              </label>
-              <input
-                type="email"
-                name="email"
-                placeholder="exemple@email.com"
-                onChange={handleChange}
-                className="w-full px-4 py-3 border border-gray-200 rounded-lg 
-                focus:outline-none focus:ring-2 focus:ring-[#0474C4]"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-sm font-medium text-[#0474C4] mb-1">
-                Mot de passe
-              </label>
-              <input
-                type="password"
-                name="password"
-                placeholder="••••••••"
-                onChange={handleChange}
-                className="w-full px-4 py-3 border border-gray-200 rounded-lg 
-                focus:outline-none focus:ring-2 focus:ring-[#0474C4]"
-                required
-              />
-            </div>
-            <div>
-              <label className="block text-sm font-medium text-[#0474C4] mb-1">
-                Confirmer le mot de passe
-              </label>
-              <input
-                type="password"
-                name="password_confirmation"
-                placeholder="••••••••"
-                onChange={handleChange}
-                className="w-full px-4 py-3 border border-gray-200 rounded-lg 
-                focus:outline-none focus:ring-2 focus:ring-[#0474C4]"
-                required
-              />
-            </div>
-            <button
-              className="w-full bg-[#0474C4] text-white py-3 rounded-lg cursor-pointer hover:bg-[#023e73] transition shadow-md">
-              S&#39;inscrire
-            </button>
-          </form>
-          <p className="text-sm text-center mt-5 text-gray-600">
-            Déjà un compte ?{" "}
-            <Link href="/connexion" className="text-[#0474C4] font-semibold underline">
-              Se connecter
-            </Link>
-          </p>
+  <div className="bg-gradient-to-br mt-27 flex justify-center">
+    <div className="flex flex-col-reverse md:flex-row w-full max-w-5xl shadow-xl rounded-2xl overflow-hidden bg-white">
 
-        </div>
+      {/* Formulaire */}
+      <div className="md:w-1/2 p-8 flex flex-col justify-center">
+        <h2 className="text-3xl font-bold text-center text-[#0474C4] mb-6">Inscription</h2>
+        <p className="text-center text-gray-500 mb-6">Créez votre compte Lost & Found</p>
+
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label className="block text-sm font-medium text-[#0474C4] mb-1">Nom complet</label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Entrez votre nom"
+              onChange={handleChange}
+              className="w-full px-4 py-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#0474C4]"
+              required
+            />
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-[#0474C4] mb-1">
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="exemple@email.com"
+              onChange={handleChange}
+              className="w-full px-4 py-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#0474C4]"
+              required/>
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-[#0474C4] mb-1">
+              Mot de passe
+            </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="••••••••"
+              onChange={handleChange}
+              className="w-full px-4 py-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#0474C4]"
+              required/>
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-[#0474C4] mb-1">
+              Confirmer le mot de passe
+            </label>
+            <input
+              type="password"
+              name="password_confirmation"
+              placeholder="••••••••"
+              onChange={handleChange}
+              className="w-full px-4 py-3 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#0474C4]"
+              required/>
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-[#0474C4] text-white py-3 rounded-lg cursor-pointer hover:bg-[#023e73] transition shadow-md"
+          >
+            S&#39;inscrire
+          </button>
+        </form>
+
+        <p className="text-sm text-center mt-5 text-gray-600">
+          Déjà un compte ?{" "}
+          <Link
+            href="/connexion"
+            className="text-[#0474C4] font-semibold underline">
+            Se connecter
+          </Link>
+        </p>
+      </div>
+
+      <div className="md:w-1/2">
+        <img
+          src="/found.jpg"
+          alt="Inscription"
+          className="w-full h-full object-cover"/>
       </div>
     </div>
-  );
+  </div>
+);
 };
 
