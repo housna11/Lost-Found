883337fe6d3d5557a4df75789a0c92c92167bd15
Housna <185698237+housna11@users.noreply.github.com>
2026-07-18 00:43:27 +0100

    Ajout de la navigation et de l'affichage conditionnel du tableau de bord

--- frontend/app/Components/Navbar.tsx
+++ frontend/app/Components/Navbar.tsx
@@ -1,6 +1,23 @@
+"use client";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+  const [role, setRole] = useState<string | null>(null);
+  const isAuthenticated = role !== null;
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (!user) return;
+    try {
+      const parsedUser = JSON.parse(user);
+      queueMicrotask(() => {
+        setRole(parsedUser.role);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
 
   return (
     <nav className="bg-gradient-to-r from-[#023e73] via-[#0474C4] to-[#7ec3ff] text-white shadow-lg">
@@ -18,24 +35,39 @@ export default function Navbar() {
             <Link href="" className="hover:opacity-80 transition font-medium">
               Déclarer
             </Link>
-            <Link href="" className="hover:opacity-80 transition font-medium">
+            <Link href="objets" className="hover:opacity-80 transition font-medium">
               Mes objets
             </Link>
-            <Link href="" className="hover:opacity-80 transition font-medium">
+            {role === "admin" && (
+            <Link href="/dashboard" className="hover:opacity-80 transition font-medium">
               Dashboard
             </Link>
+          )}
+            {!isAuthenticated ? (
             <div className="flex items-center gap-4">
               <Link
-                href=""
+                href="/connexion"
                 className="text-white px-5 py-2 rounded-lg border border-white/30 hover:bg-white hover:text-[#0474C4] transition font-medium text-base">
                 Connexion
               </Link>
+
               <Link
                 href="/inscription"
                 className="bg-white text-[#0474C4] px-5 py-2 rounded-lg font-semibold text-base hover:bg-blue-50 hover:shadow-md transition">
                 Inscription
               </Link>
             </div>
+          ) : (
+            <button
+              onClick={() => {
+                localStorage.removeItem("token");
+                localStorage.removeItem("user");
+                window.location.href = "/";
+              }}
+              className="text-white px-5 py-2 rounded-lg border border-white/30 hover:bg-white hover:text-[#0474C4] transition font-medium text-base cursor-pointer">
+              Déconnexion
+            </button>
+          )}
           </div>
         </div>
       </div>
